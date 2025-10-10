1e4303af6ac3e0c7c5a44f5d1f4e56cc64b9e4dd
Adnan Ahmad <adnanahmad9t35@gmail.com>
2025-10-10 02:45:10 -0700

    Lab 3 Control Structures

---
+++ lab3-control.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+
+    cout << "Enter a number to print its multiplication table: ";
+    cin >> num;
+
+    if (num < 0) {
+        cout << "Please enter a positive number." << endl;
+        return 0;
+    }
+
+    cout << "\n--- Multiplication Table of " << num << " ---" << endl;
+
+    for (int i = 1; i <= 10; i++) {
+        cout << num << " x " << i << " = " << num * i << endl;
+    }
+
+    cout << "-----------------------------" << endl;
+    
+    
+    
+    
+
+    int num;
+
+    cout << "Enter a number to print its multiplication table: ";
+    cin >> num;
+
+    if (num < 0) {
+        cout << "Please enter a positive number." << endl;
+        return 0;
+    }
+
+    cout << "\n--- Multiplication Table of " << num << " ---" << endl;
+
+    for (int i = 1; i <= 10; i++) {
+        cout << num << " x " << i << " = " << num * i << endl;
+    }
+
+    cout << "-----------------------------" << endl;
+
+  
+
+
+    return 0;
+}
+
