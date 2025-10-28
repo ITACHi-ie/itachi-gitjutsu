61384ad5abd8c446deb33f13beb32452693e988f
Adnan Ahmad <adnanahmad9t35@gmail.com>
2025-10-28 06:41:11 -0700

    Add task1: print name, reg no, and course name

---
+++ task1_details.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Name: Adnan Ahmad" << endl;
+    cout << "Registration Number: 23893" << endl;
+    cout << "Course Name: Software Construction and Development" << endl;
+    return 0;
+}
+
